013877fafd84d27498529fb8a325e0e4e0ed7495
aakashkhatter1296 <khatteraakash1996@gmail.com>
2018-03-14 00:02:30 +0530

    added countries data and created SearchBar Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { countries } from "../assets/countries";
+import SearchBar from "./search_bar";
 
 class App extends Component {
+
+	constructor(props){
+		super(props);
+	}
   render() {
     return (
       <div className="App">
+      	<SearchBar />
       </div>
     );
   }
---
+++ src/components/search_bar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div className="search-bar">
+				<input />
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
